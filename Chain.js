adcd05e0d737c321523dbde4db7a31656478c8cc
Jassy123 <72097730+Jassy123@users.noreply.github.com>
2021-01-02 18:00:38 +0100

    Add files via upload

--- Chain.js
+++ Chain.js
@@ -3,8 +3,8 @@ class Chain{
         var options = {
             bodyA : bodyA,
             pointB : pointB,
-            stiffness: 0.04,
-            length: 20,       
+            stiffness: 0.004,
+            length: 1,       
         }
         this.pointB = pointB;
         this.chain = Constraint.create(options);
